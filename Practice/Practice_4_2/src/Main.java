a7094530e7fe51007cca19e926cc535ee79f1cf0
Prue0115 <prue0115@gmail.com>
2025-04-19 11:32:10 +0900

    -

---
+++ Practice/Practice_4_2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 10.0;
+        double area = PI * radius * radius;
+
+        System.out.println("반지름은 " + radius + " 넓이는 " + area + " 입니다.");
+    }
+}
